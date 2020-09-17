26c32f59a7ba987f67f630a9ac6fe676c01f085a
GeraldRyan <gerald.ryan40@yahoo.com>
2020-09-17 15:02:59 -0500

    getting my portfolio page started, chosing next.js

---
+++ portfolio/src/Portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Menu } from "@material-ui/core";
+
+export default function Portfolio() {
+  return (
+    <div className="portfolio">
+      <Menu>Imported Portfolio</Menu>
+
+      <div>
+
+        Body
+      </div>
+    </div>
+  );
+}
