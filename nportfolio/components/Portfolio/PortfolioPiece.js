6afeb9557ef541ccc2fb219387389b1e322fc2f3
GeraldRyan <gerald.ryan40@protonmail.com>
2021-02-13 00:10:39 -0600

    Fixed imports blocking build

--- nportfolio/components/Portfolio/PortfolioPiece.js
+++ nportfolio/components/Portfolio/PortfolioPiece.js
@@ -5,7 +5,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/home.module.css";
 
 
 export default function PortfolioPiece({ folio, index }) {
